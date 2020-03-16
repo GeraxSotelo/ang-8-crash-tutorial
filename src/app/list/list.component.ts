04db9d0e01f2a4695e5012e70c87753b4bd0453f
Gerax Sotelo <gsotelo23@gmail.com>
2020-03-16 16:36:18 -0600

    Adding a service

--- src/app/list/list.component.ts
+++ src/app/list/list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpService } from '../http.service';
 
 @Component({
   selector: 'app-list',
@@ -7,9 +8,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ListComponent implements OnInit {
 
-  constructor() { }
+  //reference the HttpService through dependency injection
+  constructor(private _http: HttpService) { }
 
+  //ngOnInit is a life cycle hook. Runs when the component is loaded
   ngOnInit(): void {
+    this._http.myMethod();
   }
 
 }
